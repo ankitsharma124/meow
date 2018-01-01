b497b981fb8acfc9e4b2b50ef975bc3e60b4082a
Ankit Kumar Sharma <ankitsharma124@gmail.com>
2018-01-01 05:48:16 +0000

    company regiter module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { RegisterCompanyComponent } from './register-company/register-company.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterCompanyComponent
   ],
   imports: [
     BrowserModule,
